3cf03308afa7feb9134b051d65fa67aa251e20e8
김한주 <s24043@gsm.hs.kr>
2024-11-27 18:08:52 +0900

    modify :: TransConfig.java

--- src/main/java/org/example/translator/trans/config/TransConfig.java
+++ src/main/java/org/example/translator/trans/config/TransConfig.java
@@ -4,13 +4,14 @@ package org.example.translator.trans.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class TransConfig {
 
     @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
+    public WebClient webClient() {
+        return WebClient.builder().build();
     }
 
 }
